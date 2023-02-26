c901b6d9611ad209516750234aaa7a2b972ca389
alisohail2448 <alisohail2448@gmail.com>
2023-02-26 20:01:57 +0530

    initial commit

--- Lottery.sol
+++ Lottery.sol
@@ -2,11 +2,11 @@
 
 pragma solidity >=0.8.2 <0.9.0;
 
-contract Lottery{
+contract Lottery {
     address public mangager;
     address payable[] public participants;
 
-    constructor(){
+    constructor() {
         mangager = msg.sender;
     }
 
@@ -15,23 +15,31 @@ contract Lottery{
         participants.push(payable(msg.sender));
     }
 
-    function getBalance() public view returns(uint){
+    function getBalance() public view returns (uint256) {
         require(msg.sender == mangager);
         return address(this).balance;
     }
 
-    function random() public view returns(uint){
-        return uint(keccak256(abi.encodePacked(block.difficulty, block.timestamp, participants.length)));
+    function random() public view returns (uint256) {
+        return
+            uint256(
+                keccak256(
+                    abi.encodePacked(
+                        block.difficulty,
+                        block.timestamp,
+                        participants.length
+                    )
+                )
+            );
     }
-
 
     function selectWinner() public {
         require(msg.sender == mangager);
         require(participants.length >= 3);
 
-        uint r = random();
+        uint256 r = random();
         address payable winner;
-        uint index = r % participants.length;
+        uint256 index = r % participants.length;
         winner = participants[index];
         winner.transfer(getBalance());
         participants = new address payable[](0);
